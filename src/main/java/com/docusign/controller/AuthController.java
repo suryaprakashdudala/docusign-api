9587d9aad1624c10af80be0bc054840301d7e1af
suryaprakash.d <suryaprakash.d@tryalclinical.com>
2025-12-30 20:07:34 +0530

    added import

--- src/main/java/com/docusign/controller/AuthController.java
+++ src/main/java/com/docusign/controller/AuthController.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +15,7 @@ import com.docusign.entity.User;
 import com.docusign.security.JwtService;
 import com.docusign.service.EmailService;
 import com.docusign.service.UserService;
+
 import lombok.extern.slf4j.Slf4j;
 
 @RestController
